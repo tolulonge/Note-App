423df66636c83f9b6f83779e26dc7d42b64bc5c5
tolulonge <tolulopelonge945@gmail.com>
2021-11-02 03:18:45 +0100

    use cases and viewmodel implementation

--- app/src/main/java/com/tolulonge/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/tolulonge/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -1,14 +1,18 @@
 package com.tolulonge.cleanarchitecturenoteapp.feature_note.data.data_source
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.tolulonge.cleanarchitecturenoteapp.feature_note.domain.model.Note
 
 @Database(
     entities = [Note::class],
     version = 1
 )
-abstract class NoteDatabase {
+abstract class NoteDatabase : RoomDatabase() {
 
     abstract val noteDao: NoteDao
 
+    companion object{
+        const val DATABASE_NAME = "notes_db"
+    }
 }
--- app/src/main/java/com/tolulonge/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotes.kt
+++ app/src/main/java/com/tolulonge/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotes.kt
@@ -1,6 +1,38 @@
 package com.tolulonge.cleanarchitecturenoteapp.feature_note.domain.use_case
 
-class GetNotes(
+import com.tolulonge.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.tolulonge.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.tolulonge.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.tolulonge.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
+class GetNotes(
+    private val repository: NoteRepository
 ) {
+
+    operator fun invoke(noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)) : Flow<List<Note>> {
+        return repository.getNotes().map { notes ->
+            when(noteOrder.orderType){
+                is OrderType.Ascending -> {
+                    when(noteOrder){
+                        is NoteOrder.Title -> notes.sortedBy { it.title.lowercase() }
+                        is NoteOrder.Date -> notes.sortedBy { it.timeStamp }
+                        is NoteOrder.Color -> notes.sortedBy { it.color }
+
+                    }
+                }
+                is OrderType.Descending -> {
+                    when(noteOrder){
+                        is NoteOrder.Title -> notes.sortedByDescending { it.title.lowercase() }
+                        is NoteOrder.Date -> notes.sortedByDescending { it.timeStamp }
+                        is NoteOrder.Color -> notes.sortedByDescending { it.color }
+
+                    }
+                }
+            }
+
+        }
+
+    }
 }
